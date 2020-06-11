92cd304abde5d57a02d61f61597890216d66c3b3
Filipe Pfluck <filipe.pfluck@institutoivoti.com.br>
2020-06-11 19:10:05 -0300

    .

---
+++ server/src/database/seeds/createItems.ts
@@ -0,0 +1,15 @@
+import Knex from "knex"
+
+export async function seed(knex:Knex) {
+    await knex('items').insert([
+        {title: 'Lâmpadas', image: 'lampadas.svg'},
+        {title: 'Pilhas e Baterias', image: 'baterias.svg'},
+        {title: 'Papéis e Papelão', image: 'pepeis-papelao.svg'},
+        {title: 'Resíduos Eletrônicos', image: 'eletronicos.svg'},
+        {title: 'Resíduos Orgânicos', image: 'organicos.svg'},
+        {title: 'Óleo de Cozinha', image: 'oleo.svg'},
+        {title: 'Vidro', image: 'vidro.svg'},
+        {title: 'Plástico', image: 'plastico.svg'},
+        {title: 'Metal', image: 'metal.svg'},
+    ])
+}
